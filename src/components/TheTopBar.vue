819bc6e6d2de756d9e22a26587ce3af75bacaeb1
grivera <grivera@multisystems.com>
2019-08-01 15:29:37 -0400

    Changes to TOp Bar

--- src/components/TheTopBar.vue
+++ src/components/TheTopBar.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    <nav class="navbar">      
-        <a class="navbar-hamburger"><img src='../assets/hamburger-menu.svg'/></a>
-        <a v-if="language_english"><img class="navbar-logo" src='../assets/name-logo-spanish.svg'/></a>
-        <a v-else><img class="navbar-logo" src='../assets/name-logo-english.svg'/></a>
-      <ul class="nav-links" role="navigation">
-        <li><a class="btn">{{ $t("SideBar.about") }}</a></li>
-        <li><a class="btn">{{ $t("SideBar.experience") }}</a></li>
-        <li><a class="btn">{{ $t("SideBar.projects") }}</a></li>                
-        <li><a class="btn">{{ $t("SideBar.technologies") }}</a></li>                        
-        <li><a class="btn">{{ $t("SideBar.contactme") }}</a></li>               
+    <nav>
+      <!-- The hamburger class is used to draw three lines -->
+      <div class="hamburger" v-on:click="openResponsiveMenu">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>                
+      </div>
+      <ul class="nav-links">
+        <li v-if="language_english"><img /><img src="../assets/name-logo-english.svg"/></li>
+        <li v-else><img src="../assets/name-logo-spanish.svg"/></li>
+        <li><a class="nav-items" href="">{{ $t("SideBar.about") }}</a></li>
+        <li><a class="nav-items" href="">{{ $t("SideBar.experience") }}</a></li>
+        <li><a class="nav-items" href="">{{ $t("SideBar.projects") }}</a></li>        
+        <li><a class="nav-items" href="">{{ $t("SideBar.technologies") }}</a></li>
+        <li><a class="nav-items" href="">{{ $t("SideBar.contactme") }}</a></li>                
       </ul>
     </nav>
   </div>
@@ -43,6 +48,9 @@ export default {
       localStorage.language = language;        
       this.checkWhichLanguage(language);
       this.$i18n.locale = language;
+    },
+    openResponsiveMenu(){
+      $('.nav-links').toggleClass('open');
     }
   },
   mounted(){
@@ -58,52 +66,63 @@ export default {
 </script>
 <style scoped>
 nav{
-  width:100%;
-  background-color:green;
-  height:130px;
+  height:10vh;
+  background:#5b78c7;
 }
- ul{
-    list-style: none;
-    padding:0;
-  }
- @media only screen and (min-width: 909px) {
-  .navbar{
-      display: flex;
-      justify-content: space-between;
-      align-items:center;
-  }
-  .navbar-logo{
-    padding-top: 50px;
-    display:inline-block;
-  }
-  .nav-links{
-    display:flex;
-  }
-  li{
-    padding: 10px 15px;
-  }
-  .navbar-hamburger{
-    display:none;
-  }
-  .btn {
-  text-align: center;
-  padding: 15px 10px;
-  -webkit-transition-duration: 0.4s;
-  transition-duration: 0.4s;
+.nav-links{
+  display: -webkit-flex;
+  display:flex;
+}
+.nav-links li:first-child{
+  margin-right: auto;
+}
+.nav-items{
+  color:white;  
   text-decoration: none;
-  cursor: pointer;
-  border-radius: 3px;
-  color: black;
-  border: 2px solid #008CBA;
-  color: white;
+}
+ul li{
+  list-style: none;  
 }
 
-.btn:hover {
-  background-color: #008CBA;
-  color: white;
-}
- }
-  @media only screen and (max-width: 909px) {
-  
+@media screen and (max-width: 700px){
+  .line{
+    width:30px;
+    height:3px;
+    background:white;
+    margin:5px;
+  }
+  nav{
+    position: relative;
+  }
+  .hamburger{
+    position:absolute;
+    cursor: pointer;
+    right:5%;
+    top:50%;
+    transform: translate(-5%,-50%);
+    z-index: 2;
+  }
+  .nav-links{
+      position:fixed;
+      background-color: #5b78c7;
+      height: 100vh;
+      width: 100%;
+      flex-direction: column;
+      clip-path: circle(100px at 90% -10%);
+      -webkit-clip-path: circle(100px at 90% -10%);
+      transition: all 1s ease-out;
+      pointer-events: none;
+  }
+  .nav-links.open{
+      clip-path: circle(1200px at 90% -10%);
+      -webkit-clip-path: circle(1000px at 90% -10%);
+      pointer-events: all;
+  }
+  .nav-items{
+    font-size: 25px;
+  } 
+  .image{
+    display:none;
+  }
 }
 </style>
